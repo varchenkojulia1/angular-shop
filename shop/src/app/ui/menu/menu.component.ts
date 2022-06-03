31c04c98010a4996bc30bab73727b2c51e041d2c
Yuliia Varchenko <varchenkojulia1@gmail.com>
2022-06-03 19:54:12 +0300

    changes

--- shop/src/app/ui/menu/menu.component.ts
+++ shop/src/app/ui/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-ui',
+  selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
